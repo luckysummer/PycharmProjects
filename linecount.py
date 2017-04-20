c83a51b19f479a65e4c519153e1381837280fbbd
lmm <m15822270533@163.com>
2017-04-20 11:06:02 +0800

    something

---
+++ linecount.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import os, gzip
+dir_path = '/home/ll/work/work-before/literature-based similarity/compounds/DUD'
+result_dict = {}
+fileName_ls = os.listdir(dir_path)
+for fileName in fileName_ls:
+
+    filePath = os.path.join(dir_path, fileName)
+    with gzip.open(filePath, 'rb') as data:
+        count = len(data.readlines())
+        result_dict[filePath] = count
+result = sorted(result_dict.items(), key=lambda d: d[1])
+print result
+
